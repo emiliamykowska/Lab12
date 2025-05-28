f8b0c6999b680f587db7a436a56671337e6433e1
emilia <emilia.mykowska@gmail.com>
2025-05-28 09:24:20 +0200

    Commit main fixed

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        System.out.println(splitUsingPunctuation("/home/emilia/Documents/Medical Informatics/II semestr/Object_Oriented_Programming/Lab12/CleanedMetamorphosis.txt"));
+        System.out.println(splitUsingPunctuation("CleanedMetamorphosis.txt"));
 
     }
 
